ce1ac4f8343439f7c06e154aecef3179e0b26338
A <A@THINKPAD-WORKST>
2023-05-20 12:48:38 +0700

    Add project files.

---
+++ Lab3_NgoHuuTai_2087700051/Controllers/CoursesController.cs
@@ -0,0 +1,30 @@
+﻿using Lab3_NgoHuuTai_2087700051.Models;
+using Lab3_NgoHuuTai_2087700051.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Tracing;
+using System.Web.Mvc;
+
+namespace Lab3_NgoHuuTai_2087700051.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        public ActionResult Create()
+          
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ Lab3_NgoHuuTai_2087700051/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab3_NgoHuuTai_2087700051.MvcApplication" Language="C#" %>
